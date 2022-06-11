193b3d06a7ffbee4686ed0a605bb42846d89e6e0
Naoya <74296872+ce31062@users.noreply.github.com>
2022-06-11 18:38:02 +0900

    Add: Boot用USBメモリへのカーネル、ローダファイルの格納を実行
    ビルド後にこのシェルスクリプトを実行することにより、カーネルとローダをコード内で指定したストレージに適切な階層で格納することができる。
    実行コマンド: source $HOME/workspace/mikanos/kernel/usbwrite.sh

---
+++ tool/usbwrite.sh
@@ -0,0 +1,13 @@
+#Since / dev / sdf1 is a unique environment, 
+#it is necessary to check and set the path to the boot target device 
+#when using it in other environments.  
+sudo umount /dev/sdf1
+sudo mkfs.fat /dev/sdf1
+sudo mkdir -p /mnt/usbmem
+sudo mount /dev/sdf1 /mnt/usbmem
+sudo mkdir -p /mnt/usbmem/EFI/BOOT
+sudo cp /home/ubuntu/workspace/mikanos/kernel/kernel.elf /mnt/usbmem
+sudo cp $HOME/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi /mnt/usbmem/EFI/BOOT
+sudo mv /mnt/usbmem/EFI/BOOT/Loader.efi /mnt/usbmem/EFI/BOOT/BOOTX64.EFI
+
+sudo umount /mnt/usbmem 
