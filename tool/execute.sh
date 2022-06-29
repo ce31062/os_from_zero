e921ba6ad1690e137741ca5e97bf2cde6520c51e
Naoya <74296872+ce31062@users.noreply.github.com>
2022-06-29 23:01:36 +0900

    OS起動用スクリプト

--- tool/execute.sh
+++ tool/execute.sh
@@ -1,1 +1,3 @@
-$HOME/osbook/devenv/run_qemu.sh /home/ubuntu/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi /home/ubuntu/workspace/mikanos/kernel/kernel.elf 
+#$HOME/osbook/devenv/run_qemu.sh /home/ubuntu/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi /home/ubuntu/workspace/mikanos/kernel/kernel.elf 
+cd $HOME/workspace/mikanos
+APPS_DIR=apps RESOURCE_DIR=resource ./build.sh run
