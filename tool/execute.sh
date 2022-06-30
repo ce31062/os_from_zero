10bafe485d08fd38128d5296c80e7218fc305c8e
Naoya <74296872+ce31062@users.noreply.github.com>
2022-06-30 22:19:58 +0900

    Delete tool directory

--- tool/execute.sh
+++
@@ -1,4 +0,0 @@
-#$HOME/osbook/devenv/run_qemu.sh /home/ubuntu/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi /home/ubuntu/workspace/mikanos/kernel/kernel.elf 
-cd $HOME/workspace/mikanos
-source ~/osbook/devenv/buildenv.sh
-APPS_DIR=apps RESOURCE_DIR=resource ./build.sh run
