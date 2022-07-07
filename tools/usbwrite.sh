c1f5e50b188bf9a1b1f7cc26d0520cc8eed4e171
Naoya <74296872+ce31062@users.noreply.github.com>
2022-07-07 23:52:31 +0900

    day18aまでの起動メディア作成用シェルスクリプト

--- tools/usbwrite.sh
+++ tools/usbwrite.sh
@@ -6,10 +6,7 @@ sudo mkfs.fat /dev/sdf1
 sudo mkdir -p /mnt/usbmem
 sudo mount /dev/sdf1 /mnt/usbmem
 sudo mkdir -p /mnt/usbmem/EFI/BOOT
-sudo cp /home/ubuntu/workspace/mikanos/kernel/kernel.elf /mnt/usbmem
+sudo cp $HOME/workspace/mikanos/kernel/kernel.elf /mnt/usbmem
 sudo cp $HOME/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi /mnt/usbmem/EFI/BOOT
 sudo mv /mnt/usbmem/EFI/BOOT/Loader.efi /mnt/usbmem/EFI/BOOT/BOOTX64.EFI
-
-sudo cp $HOME/workspace/mikanos/apps/ /mnt/usbmem
-
 sudo umount /mnt/usbmem 
