29d24c94a9c8a2481bbe50fccab4ca67d6a1afcf
Naoya <74296872+ce31062@users.noreply.github.com>
2022-07-08 17:54:00 +0900

    起動用メディア作成用シェルスクリプト
    ./build.sh usbを実行することにより、ビルドから起動メディアまでの作成を行う。
    ./build.sh runはテキストにあるようにQEMU実行用。

---
+++ tools/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -eu
+
+make ${MAKE_OPTS:-} -C kernel kernel.elf
+
+for MK in $(ls apps/*/Makefile)
+do
+  APP_DIR=$(dirname $MK)
+  APP=$(basename $APP_DIR)
+  make ${MAKE_OPTS:-} -C $APP_DIR $APP
+done
+
+if [ "${1:-}" = "run" ]
+then
+  MIKANOS_DIR=$PWD $HOME/osbook/devenv/run_mikanos.sh
+fi
+
+if [ "${1:-}" = "usb" ]
+then
+  chmod +x usbwrite_with_apps.sh
+  $HOME/workspace/mikanos/usbwrite_with_apps.sh
+fi
