c7f35cc1889c6976def9322e4b270a260dacf3de
Naoya <74296872+ce31062@users.noreply.github.com>
2022-07-09 01:19:20 +0900

    day18b以降の起動メディア作成用シェルスクリプト
    build.shの第一引数にusbを設定することにより呼び出しされる。
    USBメモリ等の起動メディアまでのパスはスクリプト内で指定する必要あり。
    順次改善予定。

--- tools/usbwrite_with_apps.sh
+++ tools/usbwrite_with_apps.sh
@@ -1,18 +1,23 @@
 #This script is executed when volume strage like as usb memory.
 #Change the boot storage path according to the environment.
+BOOT_STRAGE="/dev/sdf1"
 
 cd $HOME/workspace/mikanos
-source ~/osbook/devenv/buildenv.sh
-source build.sh
+
 sudo mount -o loop disk.img mnt
 
-sudo umount /dev/sdf1
-sudo mkfs.fat /dev/sdf1
+sudo umount $BOOT_STRAGE
+sudo mkfs.fat $BOOT_STRAGE
 sudo mkdir -p /mnt/usbmem
-sudo mount /dev/sdf1 /mnt/usbmem
+sudo mount $BOOT_STRAGE /mnt/usbmem
 sudo mkdir -p /mnt/usbmem/EFI/BOOT
-sudo cp $HOME/workspace/mikanos/mnt/kernel.elf /mnt/usbmem
-sudo cp $HOME/workspace/mikanos/mnt/EFI/BOOT/BOOTX64.EFI /mnt/usbmem/EFI/BOOT
-sudo cp $HOME/workspace/mikanos/mnt/onlyhlt /mnt/usbmem
+sudo cp -r $HOME/workspace/mikanos/mnt/* /mnt/usbmem
+
+#sudo cp $HOME/workspace/mikanos/mnt/kernel.elf /mnt/usbmem
+#sudo cp $HOME/workspace/mikanos/mnt/EFI/BOOT/BOOTX64.EFI /mnt/usbmem/EFI/BOOT
+#sudo cp $HOME/workspace/mikanos/mnt/onlyhlt /mnt/usbmem
+#sudo cp $HOME/workspace/mikanos/mnt/rpn /mnt/usbmem
+
 sudo umount /mnt/usbmem
+
 
