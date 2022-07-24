4d4e1dbd43160aedb2492c078cfb8e70ec499bcd
Naoya <74296872+ce31062@users.noreply.github.com>
2022-07-24 20:55:38 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,3 +13,8 @@ if [ "${1:-}" = "run" ]
 then
   MIKANOS_DIR=$PWD $HOME/osbook/devenv/run_mikanos.sh
 fi
+if [ "${1:-}" = "usb" ]
+then
+  chmod +x usbwrite_with_apps.sh
+  $HOME/workspace/mikanos/usbwrite_with_apps.sh
+fi
